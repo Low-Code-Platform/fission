3994aad502e379340e3b88a813ab000dad61da0c
Prasad <prasad.n@syncoms.com>
2023-07-05 15:52:09 +0530

    private npm config and shell update

--- environments/nodejs/builder/build.sh
+++ environments/nodejs/builder/build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 cd ${SRC_PKG}
+if [[ -n "$NPM_TOKEN" ]] && [[ -n "$NPM_REGISTRY" ]]; then
+    npm set @sys:registry https://${NPM_REGISTRY}
+    npm set //${NPM_REGISTRY}/:_authToken ${NPM_TOKEN}
+fi
 npm install && cp -r ${SRC_PKG} ${DEPLOY_PKG}
